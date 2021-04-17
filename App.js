e2b41f5fd510dc000c177b05267de933768bbbb5
Eliseu Damasceno <eliseudamasceno@gmail.com>
2021-04-17 00:49:36 -0300

    Created the Password Generator app

--- App.js
+++ App.js
@@ -1,21 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+import Slider from '@react-native-community/slider';
+import Clipboard from 'expo-clipboard';
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
 export default function App() {
+  const [password, setPassword] = useState('');
+  const [size, setSize] = useState(10);
+
+  function generatePass() {
+    let pass = '';
+    for (let i = 0, n = charset.length; i < size; i++) {
+      pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    setPassword(pass);
+  }
+
+  function copyPass() {
+    Clipboard.setString(password);
+    alert('Password successfully copied!')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./src/assets/logo.png')}
+        style={styles.logo}
+      />
+
+      <Text style={styles.title}>{size} Caracters</Text>
+
+      <View style={styles.area}>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor='#ff0000'
+          maximumTrackTintColor='#000'
+          value={size}
+          onValueChange={(value) => setSize(value.toFixed(0))}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={generatePass}>
+        <Text style={styles.buttonText}>Generate Password</Text>
+      </TouchableOpacity>
+
+      {password !== '' && (
+        <View style={styles.area}>
+          <Text
+            style={styles.password}
+            onLongPress={copyPass}
+          >
+            {password}
+          </Text>
+        </View>
+      )}
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f3f3f3',
   },
+  logo: {
+    marginBottom: 60,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  area: {
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: '#fff',
+    width: '80%',
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#ffa200',
+    width: '80%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+    marginBottom: 25,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  password: {
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 20,
+  }
 });
